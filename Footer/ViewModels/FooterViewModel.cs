5ec63e0a9686f2659bcee84ed7c53726ea112089
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-30 14:06:06 +0100

    Create FooterViewModel with navigation handling, handling navigation props and change property

---
+++ Footer/ViewModels/FooterViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Mvvm;
+using Prism.Regions;
+
+namespace Footer.ViewModels
+{
+    class FooterViewModel : BindableBase, INavigationAware
+    {
+        private string? _imgUrl;
+        private readonly IRegionManager _regionManager;
+
+        public FooterViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public string? ImageUrl
+        {
+            get { return _imgUrl; }
+            set { SetProperty(ref _imgUrl, value); }
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            if (navigationContext.Parameters.ContainsKey("imgUrl"))
+            {
+                ImageUrl = navigationContext.Parameters.GetValue<string>("imgUrl");
+            }
+        }
+    }
+}

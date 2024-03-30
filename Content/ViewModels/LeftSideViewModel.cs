7535e98c34928ea58abee33608bdc7afbac8a323
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-30 13:15:52 +0100

    Create the left side view model and the view xaml

---
+++ Content/ViewModels/LeftSideViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.ViewModels
+{
+    class LeftSideViewModel : BindableBase
+    {
+        private string? _date;
+        private string? _imageVisibility = "Hidden";
+
+        public string? Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public string? ImageVisibility
+        {
+            get { return _imageVisibility; }
+            set { SetProperty(ref _imageVisibility, value); }
+        }
+
+        public LeftSideViewModel()
+        {
+            Date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}

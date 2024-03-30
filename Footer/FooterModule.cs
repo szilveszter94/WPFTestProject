04e89bde93c040e6483e9aad3dd354f3a2b8a542
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-30 14:04:54 +0100

    Register Footer Module for navigation in the FooterModule class

---
+++ Footer/FooterModule.cs
@@ -0,0 +1,18 @@
+﻿using Footer.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace Footer
+{
+    public class FooterModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<FooterView>();
+        }
+    }
+}

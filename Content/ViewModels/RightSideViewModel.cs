70c948c59ef7c97abb5e83656da2582bf3c156e7
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-30 13:14:35 +0100

    Create the right side view model and the view xaml

---
+++ Content/ViewModels/RightSideViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Mvvm;
+using Prism.Regions;
+
+namespace Content.ViewModels
+{
+    class RightSideViewModel : BindableBase
+    {
+        private string? _imgUrl;
+        private string? _imageVisibility = "Hidden";
+
+        public string? ImageVisibility
+        {
+            get { return _imageVisibility; }
+            set { SetProperty(ref _imageVisibility, value); }
+        }
+
+        public string? ImageUrl
+        {
+            get { return _imgUrl; }
+            set { SetProperty(ref _imgUrl, value); }
+        }
+    }
+}

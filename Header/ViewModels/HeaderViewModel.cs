32e4299ad7ab543a8cb1c0ff5ff54183183db8db
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-30 12:46:29 +0100

    Refactor the HeaderViewModel and add a command for showing the dialog

--- Header/ViewModels/HeaderViewModel.cs
+++ Header/ViewModels/HeaderViewModel.cs
@@ -1,13 +1,30 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
 
 namespace Header.ViewModels
 {
     public class HeaderViewModel : BindableBase
     {
-        private string _openImageButtonText = "Kép megnyitása";
-        private string _profileButtonText = "Névjegy";
-
+        public DelegateCommand ShowDialogCommand { get; }
         public string OpenImageButtonText { get { return _openImageButtonText; } set { _openImageButtonText = value; } }
         public string ProfileButtonText { get { return _profileButtonText; } set { _profileButtonText = value; } }
+
+        private string _openImageButtonText = "Kép megnyitása";
+        private string _profileButtonText = "Névjegy";
+        private readonly IDialogService _dialogService;
+
+        
+        public HeaderViewModel(IDialogService dialogService)
+        {
+            ShowDialogCommand = new DelegateCommand(ShowDialog);
+            _dialogService = dialogService;
+        }
+
+        private void ShowDialog()
+        {
+            _dialogService.ShowDialog("MessageDialog");
+        }
+
     }
 }

8be02b475c49b267c0d7c522bd9dcb4d742165d4
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-30 12:27:51 +0100

    Create the header module

---
+++ Header/HeaderModule.cs
@@ -0,0 +1,30 @@
+﻿using Header.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Header
+{
+    public class HeaderModule : IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+        public HeaderModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            _regionManager.RegisterViewWithRegion("header", typeof(HeaderView));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}

364f964c42d6bae97a1be8b218e07ac5a89f7f89
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-30 12:28:10 +0100

    Create the header view model

---
+++ Header/ViewModels/HeaderViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Mvvm;
+
+namespace Header.ViewModels
+{
+    public class HeaderViewModel : BindableBase
+    {
+        private string _openImageButtonText = "Kép megnyitása";
+        private string _profileButtonText = "Névjegy";
+
+        public string OpenImageButtonText { get { return _openImageButtonText; } set { _openImageButtonText = value; } }
+        public string ProfileButtonText { get { return _profileButtonText; } set { _profileButtonText = value; } }
+    }
+}

7cc44c581aa19ab004ab319fccbcd8d4adcc26e7
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-30 12:44:43 +0100

    Create the MessageDialogViewModel class and customize it

---
+++ WPFTestProject/Dialogs/MessageDialogViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTestProject.Dialogs
+{
+    public class MessageDialogViewModel : BindableBase, IDialogAware
+    {
+        public string Title => "Névjegy";
+        public string Message { get; } = "Hello World!";
+
+        public DelegateCommand CloseDialogCommand { get; }
+
+        public event Action<IDialogResult> RequestClose;
+
+        public MessageDialogViewModel()
+        {
+            CloseDialogCommand = new DelegateCommand(CloseDialog);
+        }
+
+        private void CloseDialog()
+        {
+            var buttonResult = ButtonResult.OK;
+
+            RequestClose?.Invoke(new DialogResult(buttonResult));
+        }
+
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+
+        }
+    }
+}

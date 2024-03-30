2112657f1f41d5cbe30db1a11f509368973af31a
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-30 12:12:23 +0100

    Create the layout of the application.

--- WPFTestProject/ViewModels/MainWindowViewModel.cs
+++ WPFTestProject/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace WPFTestProject.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "WPF Tesztfeladat";
         public string Title
         {
             get { return _title; }
